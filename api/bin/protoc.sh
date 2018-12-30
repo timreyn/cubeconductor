39ecc2f9f8434346bb62016d1d64f11bd94bb2d0
Tim Reynolds <tim@cubingusa.org>
2018-12-30 12:11:00 -0500

    Flesh out WCIF protos.

--- api/bin/protoc.sh
+++ api/bin/protoc.sh
@@ -6,7 +6,7 @@
 WATCH=0
 
 run_protoc() {
-  protoc --proto_path=api --python_out=server/src/api api/*.proto
+  protoc --proto_path=api --python_out=server/src/api api/wcif/*.proto
 }
 
 while getopts "w" opt; do
@@ -20,9 +20,9 @@ run_protoc
 if [[ $WATCH == 1 ]]; then
   echo "Watching for changes."
   while [[ true ]]; do
-    if [[ $(find api/*.proto -type f -mmin -0.05) != "" ]] ; then
+    if [[ $(find api -type f -mmin -0.05 -name *.proto) != "" ]] ; then
       echo "Change detected, re-compiling protos:"
-      find api/*.proto -type f -mmin -0.05
+      find api -type f -mmin -0.05 -name *.proto 
       run_protoc && echo "Changes compiled!"
       echo ""
     fi
