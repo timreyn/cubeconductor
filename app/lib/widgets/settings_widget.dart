e28929b606238838431aacd5f08b81196e95158d
Tim Reynolds <tim@cubingusa.org>
2019-01-06 17:32:04 -0500

    Show a toast when settings are saved.

--- app/lib/widgets/settings_widget.dart
+++ app/lib/widgets/settings_widget.dart
@@ -12,45 +12,50 @@ class SettingsWidget extends StatelessWidget {
       : _formKey = new GlobalKey(),
         super(key: key);
 
-  void submit() {
+  void submit(BuildContext context) {
     if (this._formKey.currentState.validate()) {
       _formKey.currentState.save();
+
+      Scaffold.of(context)
+          .showSnackBar(new SnackBar(content: new Text("Settings updated!")));
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: buildAppBar(context, defaultMenuItems, "Settings"),
-      body: new Container(
-        padding: const EdgeInsets.all(8.0),
-        child: new Form(
-          key: _formKey,
-          child: new ListView(
-            children: <Widget>[
-              new TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Server URL",
+        appBar: buildAppBar(context, defaultMenuItems, "Settings"),
+        body: Builder(
+          builder: (context) => new Container(
+                padding: const EdgeInsets.all(16.0),
+                child: new Form(
+                  key: _formKey,
+                  child: new ListView(
+                    children: <Widget>[
+                      new TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: "Server URL",
+                        ),
+                        initialValue:
+                            getPreference(sharedPreferences, Prefs.serverUrl),
+                        onSaved: (String value) {
+                          setStringPreference(
+                              sharedPreferences, Prefs.serverUrl, value);
+                        },
+                      ),
+                      new RaisedButton(
+                        child: new Text(
+                          'Submit',
+                          style: new TextStyle(color: Colors.white),
+                        ),
+                        color: Colors.blue,
+                        onPressed: () => this.submit(context),
+                      ),
+                    ],
+                  ),
                 ),
-                initialValue: getPreference(sharedPreferences, Prefs.serverUrl),
-                onSaved: (String value) {
-                  setStringPreference(
-                      sharedPreferences, Prefs.serverUrl, value);
-                },
               ),
-              new RaisedButton(
-                child: new Text(
-                  'Submit',
-                  style: new TextStyle(color: Colors.white),
-                ),
-                color: Colors.blue,
-                onPressed: this.submit,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+        ));
   }
 
   final List<MenuItem> defaultMenuItems;
